88422ca04a29da704f0076e42787aa95be38e439
Roman Fedorchuk <Romaitgo@gmail.com>
2025-01-15 04:19:51 +0100

    rea

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import FriendList from './components/friendList/FriendList';
-import Profile from './components/profile/Profile';
 import userData from './userData.json';
 import friends from './friends.json';
 import transactions from './transactions.json';
 import TransactionHistory from './components/transactionHistory/TransactionHistory';
+import Profile from './components/profile/Profile';
 
 const App = () => {
   return (
